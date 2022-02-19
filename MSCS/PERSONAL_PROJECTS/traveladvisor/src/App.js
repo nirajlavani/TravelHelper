fa983a34a25da296271dcf0368ff20ac6094a99d
Niraj Lavani <60410421+nirajlavani@users.noreply.github.com>
2022-02-18 20:33:41 -0500

    fetch restaurant data when map moves

--- MSCS/PERSONAL_PROJECTS/traveladvisor/src/App.js
+++ MSCS/PERSONAL_PROJECTS/traveladvisor/src/App.js
@@ -20,9 +20,7 @@ const App = () => {
     }, []);
 
     useEffect(() => {
-        console.log(coordinates, bounds);
-
-        getPlacesData()
+        getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
                 console.log(data);
                 setPlaces(data);
